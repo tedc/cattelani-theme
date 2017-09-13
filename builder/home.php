95e86fb34158fc1773ae0387b881be52a88e646f
tedc <egrandinetti@gmail.com>
2017-09-13 12:25:43 +0200

    size

--- builder/home.php
+++ builder/home.php
@@ -10,7 +10,7 @@
 		);
 		foreach($terms as $term) :
 	?>
-		<ks-swiper-slide class="swiper-slide" ng-style="{'background-image':'url(<?php echo get_field('cover_image', 'collezioni_'.$term->term_id)['url']; ?>)'}">
+		<ks-swiper-slide class="swiper-slide" ng-style="{'background-image':'url(<?php echo get_field('cover_image', 'collezioni_'.$term->term_id)['url']; ?>)', 'background-size' : 'cover'}">
 		</ks-swiper-slide>
 	<?php endforeach; ?>
 	</ks-swiper-container>
