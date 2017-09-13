7faa227e503ac9d18b70263f0d2a0fa68a34714e
tedc <egrandinetti@gmail.com>
2017-09-13 12:29:26 +0200

    remove loop

--- builder/home.php
+++ builder/home.php
@@ -1,5 +1,5 @@
 <div class="collections" ng-swiper>
-	<ks-swiper-container class="collections__slider" swiper="main" loop="true" override-parameters="{'effect':'fade', 'fade' : {'crossFade':true}}" on-ready="sliderReady(main)">
+	<ks-swiper-container class="collections__slider" swiper="main" override-parameters="{'effect':'fade', 'fade' : {'crossFade':true}}" on-ready="sliderReady(main)">
 	<?php 
 		$terms = get_terms(
 			array(
@@ -16,7 +16,7 @@
 	</ks-swiper-container>
 	<div class="collections__nav collections__nav--shrink collections__nav--grow-md" ng-class="{'collections__nav--active' : navInit}">
 	<i class="icon-arrow icon-arrow-prev"></i>
-	<ks-swiper-container class="collections__container collections__container--shrink" swiper="nav" loop="true" slides-per-view="'auto'" override-parameters="{'centeredSlides':true,'loopedSlides':3, 'slideToClickedSlide' : true, 'nextButton' : '.collections__nav .icon-arrow-next', 'prevButton' : '.collections__nav .icon-arrow-prev'}">
+	<ks-swiper-container class="collections__container collections__container--shrink" swiper="nav" slides-per-view="'auto'" override-parameters="{'centeredSlides':true,'loopedSlides':3, 'slideToClickedSlide' : true, 'nextButton' : '.collections__nav .icon-arrow-next', 'prevButton' : '.collections__nav .icon-arrow-prev'}">
 		<?php $index = 0; foreach($terms as $term) : ?>
 		<ks-swiper-slide class="swiper-slide">
 			<span class="swiper-slide__item">
