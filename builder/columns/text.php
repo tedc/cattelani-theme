ac1b9fbe3a02c2d20fd285ab5936741515b1d43e
tedc <egrandinetti@gmail.com>
2017-09-18 20:26:11 +0200

    section shrink

--- builder/columns/text.php
+++ builder/columns/text.php
@@ -1,4 +1,4 @@
-<div class="section__content section__content--grow-lg section__content--shrink-<?php echo ($col%2==0) ? 'right' : 'left'; ?>-only">
+<div class="section__content section__content--grow-lg">
 	<?php if(get_sub_field('random_quote')) :
 	include(locate_template( 'builder/columns/aforisma.php', false, true ));
 	else :?>
@@ -7,15 +7,15 @@
 			<?php the_sub_field('title_text'); ?>
 		</h4>
 	<?php } if(get_sub_field('text')) {?>
-	<div class="section__text<?php echo (get_sub_field('title_text')) ? ' section__text--grow-md-top' : ''; echo (get_sub_field('col_font')>0) ? ' section__text--alternate': ''; ?>">
+	<div class="section__text section__text--shrink-<?php echo ($col%2==0) ? 'right' : 'left'; ?>-only<?php echo (get_sub_field('title_text')) ? ' section__text--grow-md-top' : ''; echo (get_sub_field('col_font')>0) ? ' section__text--alternate': ''; ?>">
 		<?php the_sub_field('text'); ?>
 	</div>
 	<?php } if(get_sub_field('col_link')) : ?>
-	<div class="section__link section__link--grow-md-top">
+	<div class="section__link section__link--grow-md-top section__link--shrink-<?php echo ($col%2==0) ? 'right' : 'left'; ?>-only">
 		<a href="<?php the_sub_field('col_link'); ?>" ui-sref="app.page({slug : '<?php echo basename(get_sub_field('col_link')); ?>'})" class="section__send"><?php the_sub_field('link_text'); ?></a>
 	</div>
 	<?php endif; if(get_sub_field('col_sign')) : ?>
-		<p class="section__sign section__sign--grow-md-top section__sign--lighter"><?php the_sub_field('col_sign'); ?></p>
+		<p class="section__sign section__sign--grow-md-top section__sign--lighter section__link--shrink-<?php echo ($col%2==0) ? 'right' : 'left'; ?>-only"><?php the_sub_field('col_sign'); ?></p>
 	<?php endif; endif; ?>
 <div class="section__mask"></div>
 </div>
