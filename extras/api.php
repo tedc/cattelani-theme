b2b754f368cc528ea5c36cc6328c48d76955cbd0
tedc <egrandinetti@gmail.com>
2017-09-21 15:56:33 +0200

    remove error

--- extras/api.php
+++ extras/api.php
@@ -42,33 +42,6 @@
         function lampade_totali() {
           return get_posts(array('posts_per_page' => -1, 'post_type' => 'lampade', 'orderby' => 'title', 'order'=>'ASC'));
         }
-        function instagram_posts() {
-            $client_id = get_field('instagram_client_id', 'options');
-            $client_secret = get_field('instagram_client_secret', 'options');
-            $access_token = get_field('instagram_access_token', 'options');
-            $user_id = get_field('instagram_user_id', 'options');
-            $count = get_field('instagram_count', 'options');
-            $api = new Instaphp\Instaphp([
-                    'client_id' => $client_id,
-                    'client_secret' => $client_secret,
-                    'redirect_uri' => get_bloginfo('url'),
-                    'http_timeout' => 6000,
-                    'http_connect_timeout' => 2000
-                ]);
-            if(!$api) {
-                return;
-            }
-            $api->setAccessToken($access_token);
-            $items = $api->Users->Recent($user_id, array('count'=>$count));
-            $cached = get_transient($user_id);
-            if($cached !== false) {
-                return $cached;
-            } else {
-                $expiration_time = 60*60*2;
-                set_transient($user_id, $items, $expiration_time);
-                return $items;
-            }
-        }
        //  register_api_field(  array('post', 'lampade', 'progetti', 'installazioni', 'page'),
        //     'post_thumbnail',
        //     array(
