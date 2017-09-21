bea50e839e66f0b61435abcb7e2b37e57fece055
tedc <egrandinetti@gmail.com>
2017-09-21 17:16:26 +0200

    params['per_page']

--- extras/api.php
+++ extras/api.php
@@ -626,7 +626,7 @@ add_filter( 'rest_post_collection_params', 'my_prefix_change_post_per_page', 10,
 
 function my_prefix_change_post_per_page( $params ) {
     if ( isset( $params['per_page'] ) ) {
-        $params['per_page']['maximum'] = $params['per_page']
+        $params['per_page']['maximum'] = $params['per_page'];
     }
 
     return $params;
