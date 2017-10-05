3c41f1dc53e00c217ef3a37d7d1fe6f080b9f44e
tedc <egrandinetti@gmail.com>
2017-10-05 18:28:22 +0200

    prova

--- extras/setup.php
+++ extras/setup.php
@@ -441,33 +441,9 @@
 	}
 	add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 
-	add_action('restrict_manage_posts', 'tsm_filter_post_type_by_taxonomy');
-	function tsm_filter_post_type_by_taxonomy() {
-		global $typenow;
-		$post_type = 'lampade'; // change to your post type
-		$taxonomy  = 'collezioni'; // change to your taxonomy
-		if ($typenow == $post_type) {
-			$selected      = isset($_GET[$taxonomy]) ? $_GET[$taxonomy] : '';
-			$info_taxonomy = get_taxonomy($taxonomy);
-			wp_dropdown_categories(array(
-				'show_option_all' => __("Mostra tutte le collezioni", 'catellani'),
-				'taxonomy'        => $taxonomy,
-				'name'            => $taxonomy,
-				'orderby'         => 'name',
-				'selected'        => $selected,
-				'show_count'      => true,
-				'hide_empty'      => true,
-			));
-		};
-	}
-	// add_filter('parse_query', 'tsm_convert_id_to_term_in_query');
-	// function tsm_convert_id_to_term_in_query($query) {
-	// 	global $pagenow;
-	// 	$post_type = 'lampade'; // change to your post type
-	// 	$taxonomy  = 'collezioni'; // change to your taxonomy
-	// 	$q_vars    = &$query->query_vars;
-	// 	if ( $pagenow == 'edit.php' && isset($q_vars['post_type']) && $q_vars['post_type'] == $post_type && isset($q_vars[$taxonomy]) && is_numeric($q_vars[$taxonomy]) && $q_vars[$taxonomy] != 0 ) {
-	// 		$term = get_term_by('id', $q_vars[$taxonomy], $taxonomy);
-	// 		$q_vars[$taxonomy] = $term->slug;
-	// 	}
-	// }
+	include_once('order_by_tax.php');
+	new Tax_CTP_Filter(
+	   array(
+	      'lampade' => array('collezioni')
+	   ) 
+	);
