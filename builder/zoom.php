26d34ec246a30a76efc623f846ef4a53ffac31e2
tedc <egrandinetti@gmail.com>
2017-10-11 16:23:54 +0200

    fix zoom

--- builder/zoom.php
+++ builder/zoom.php
@@ -1,4 +1,4 @@
-<div class="zoom zoom--shrink-fw" ng-click="isZoom[<?php echo $row; ?>]=!isZoom[<?php echo $row; ?>];updateScrollbar(isZoom[<?php echo $row; ?>], <?php echo get_sub_field('zoom_image')['width'] ?>, <?php echo get_sub_field('zoom_image')['height']; ?>)" ng-class="{'zoom--active' : isZoom[<?php echo $row; ?>]}" ng-zoom id="zoom_<?php echo $row; ?>" ng-mousemove="cursor($event)">
+<div class="zoom zoom--shrink-fw" ng-click="isZoom[<?php echo $row; ?>]=true;updateScrollbar(isZoom[<?php echo $row; ?>], <?php echo get_sub_field('zoom_image')['width'] ?>, <?php echo get_sub_field('zoom_image')['height']; ?>)" ng-class="{'zoom--active' : isZoom[<?php echo $row; ?>]}" ng-zoom id="zoom_<?php echo $row; ?>" ng-mousemove="cursor($event)">
 	<header class="zoom__header zoom__header--shrink"><h4 class="zoom__title zoom__title--larger-light"><?php _e('Guarda i dettagli di'); ?> <?php the_title(); ?></h4></header>
 	<div class="zoom__container">
 		<figure class="zoom__figure" ng-style="{'background-image' : 'url(<?php echo get_sub_field('zoom_preview') ? get_sub_field('zoom_preview')['url'] : get_sub_field('zoom_image')['url']; ?>)'}">
@@ -8,8 +8,11 @@
 			<figcaption class="zoom__cursor">
 				<i class="zoom__icn"></i>
 				<span class="zoom__open"><?php _e('Clicca ed esplora', 'catellani'); ?></span>
-				<span class="zoom__close"><?php _e('Chiudi', 'catellani'); ?></span>
-			</figcaption>	
+				<span class="zoom__drag"><?php _e('Trascina ed esplora', 'catellani'); ?></span>
+			</figcaption>
+			<span class="zoom__close zoom__close--shrink zoom__close--grow-md" ng-click="isZoom[<?php echo $row; ?>]=false">
+				<i class="icon-close"></i><?php _e('Chiudi', 'catellani'); ?>
+			</span>
 		</figure>
 	</div>
 </div>
