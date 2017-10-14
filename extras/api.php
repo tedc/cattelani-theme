b2ebdf9ed02b5dd6f3dc7587e8660105b3c16071
tedc <egrandinetti@gmail.com>
2017-10-14 15:11:11 +0200

    $object['id']

--- extras/api.php
+++ extras/api.php
@@ -200,7 +200,7 @@
         function add_post_thumb($object) {
            $id = get_post_thumbnail_id($object['id']);
            $alt = get_post_meta($id, '_wp_attachment_image_alt', true);
-           return array('id'=>$id, 'alt' => $alt, 'magazine' => get_the_post_thumbnail_url( $id, 'magazine' ), 'large' => get_the_post_thumbnail_url( $id, 'large' ), 'full' => get_the_post_thumbnail_url( $id, 'full' ));
+           return array('id'=>$id, 'alt' => $alt, 'magazine' => get_the_post_thumbnail_url( $object['id'], 'magazine' ), 'large' => get_the_post_thumbnail_url( $object['id'], 'large' ), 'full' => get_the_post_thumbnail_url( $object['id'], 'full' ));
         }// POST THUMBNAIL
     
 
