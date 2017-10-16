63de93a42bbf007a0e984c7bb39066b0b0212ff8
tedc <egrandinetti@gmail.com>
2017-10-16 19:28:11 +0200

    move

--- builder/zoom.php
+++ builder/zoom.php
@@ -1,16 +1,16 @@
-<div class="zoom zoom--shrink-fw" ng-click="isZoom[<?php echo $row; ?>]=true;updateScrollbar(isZoom[<?php echo $row; ?>], <?php echo get_sub_field('zoom_image')['width'] ?>, <?php echo get_sub_field('zoom_image')['height']; ?>)" ng-class="{'zoom--active' : isZoom[<?php echo $row; ?>]}" ng-zoom id="zoom_<?php echo $row; ?>" ng-mousemove="cursor($event)">
+<div class="zoom zoom--shrink-fw" ng-class="{'zoom--active' : isZoom[<?php echo $row; ?>]}" ng-zoom id="zoom_<?php echo $row; ?>">
 	<header class="zoom__header zoom__header--shrink"><h4 class="zoom__title zoom__title--larger-light"><?php _e('Guarda i dettagli'); ?></h4></header>
-	<div class="zoom__container">
+	<div class="zoom__container" ng-mousemove="cursor($event)" ng-mouseleave="leave()" ng-click="isZoom[<?php echo $row; ?>]=true;updateScrollbar(isZoom[<?php echo $row; ?>], <?php echo get_sub_field('zoom_image')['width'] ?>, <?php echo get_sub_field('zoom_image')['height']; ?>)">
 		<figure class="zoom__figure" ng-style="{'background-image' : 'url(<?php echo get_sub_field('zoom_preview') ? get_sub_field('zoom_preview')['url'] : get_sub_field('zoom_image')['url']; ?>)'}">
 			<div class="zoom__scroll">
 				<?php echo wp_get_attachment_image( get_sub_field('zoom_image')['ID'], 'full', false, array('class' => 'zoom__image')); ?>
 			</div>
-			<figcaption class="zoom__cursor">
+			<figcaption class="zoom__cursor" ng-class="{'zoom__cursor--hidden':closeHover}">
 				<i class="zoom__icn"></i>
 				<span class="zoom__open"><?php _e('Clicca ed esplora', 'catellani'); ?></span>
 				<span class="zoom__drag"><?php _e('Trascina ed esplora', 'catellani'); ?></span>
 			</figcaption>
-			<span class="zoom__close zoom__close--shrink zoom__close--grow-md" ng-click="$event.stopPropagation();isZoom[<?php echo $row; ?>]=false">
+			<span class="zoom__close zoom__close--shrink zoom__close--grow-md" ng-click="$event.stopPropagation();isZoom[<?php echo $row; ?>]=false" ng-mouseenter="closeHover = true" ng-mouseleave="closeHover = false">
 				<i class="icon-close"></i><?php _e('Chiudi', 'catellani'); ?>
 			</span>
 		</figure>
