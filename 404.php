750039dbeae1836d50e3b4daae09108726afcf4f
tedc <egrandinetti@gmail.com>
2017-10-17 20:00:11 +0200

    404

--- 404.php
+++ 404.php
@@ -1,16 +1,15 @@
 <div class="error error--grid" ng-error-page>
 	<div class="error__cell error__cell--image">
-		<figure class="error__image error__image--off" ng-style="{backgroundImage : 'url(<?php echo get_stylesheet_directory_uri() . '/assets/images/404-off.jpg' ?>)'}">
-			<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/404-on.jpg' ?>" alt="<?php echo get_bloginfo('name'); ?> - 404">
-		</figure>
 		<div class="error__image error__image--on" ng-style="{backgroundImage : 'url(<?php echo get_stylesheet_directory_uri() . '/assets/images/404-on.jpg' ?>)'}">
 		</div>
-		<nav class="error__link">
+		<figure class="error__image error__image--off">
+			<div class="error__contain" ng-style="{backgroundImage : 'url(<?php echo get_stylesheet_directory_uri() . '/assets/images/404-off.jpg' ?>)'}"></div>
+			<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/404-on.jpg' ?>" alt="<?php echo get_bloginfo('name'); ?> - 404">
 			<a href="<?php echo home_url(); ?>" ng-click="goToHome()">
 				<?php _e('Torna alla', 'catellani'); ?><br />
 				<?php _e('Home', 'catellani'); ?>
 			</a>
-		</nav>
+		</figure>
 	</div>
 	<div class="error__cell error__cell--content">
 		<h1 class="error__title error__title--huge-lighter">
