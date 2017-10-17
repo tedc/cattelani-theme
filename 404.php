261061b84d5a93abc1b8d0562fce78a98a85e3a5
tedc <egrandinetti@gmail.com>
2017-10-17 13:10:43 +0200

    404

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <div class="error error--grid" ng-error-page>
 	<div class="error__cell error__cell--image">
-		<figure class="error__image error__image--off" ng-style="{backgroundImage : 'url(<?php echo get_stylesheet_directory_uri() . '/assets/images/404-off.png' ?>)'}">
-			<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/404-on.png' ?>" alt="<?php echo get_bloginfo('name'); ?> - 404">
+		<figure class="error__image error__image--off" ng-style="{backgroundImage : 'url(<?php echo get_stylesheet_directory_uri() . '/assets/images/404-off.jpg' ?>)'}">
+			<img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/404-on.jpg' ?>" alt="<?php echo get_bloginfo('name'); ?> - 404">
 		</figure>
-		<div class="error__image error__image--on" ng-style="{backgroundImage : 'url(<?php echo get_stylesheet_directory_uri() . '/assets/images/404-on.png' ?>)'}">
+		<div class="error__image error__image--on" ng-style="{backgroundImage : 'url(<?php echo get_stylesheet_directory_uri() . '/assets/images/404-on.jpg' ?>)'}">
 		</div>
 		<a href="<?php echo home_url(); ?>" ng-click="goToHome()">
 			<?php _e('Torna alla', 'catellani'); ?><br />
