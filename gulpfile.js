efb2f59c747b77d10a5f1b028f35f597da7be4c9
tedc <egrandinetti@gmail.com>
2017-10-18 11:42:20 +0200

    again

--- gulpfile.js
+++ gulpfile.js
@@ -396,7 +396,9 @@ gulp.task('watch', function() {
 // Generally you should be running `gulp` instead of `gulp build`.
 gulp.task('build', function(callback) {
     runSequence('styles',
+        'tpl',
         'scripts',
+        'Iconfont',
         ['fonts', 'images'],
         callback);
 });
