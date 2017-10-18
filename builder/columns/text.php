6b0c1d58f4c9fe1a7bc9ba99bde25e9d6110cf5e
tedc <egrandinetti@gmail.com>
2017-10-18 18:47:27 +0200

    text

--- builder/columns/text.php
+++ builder/columns/text.php
@@ -17,7 +17,7 @@
 	<div class="section__text section__text--shrink-<?php echo ($col%2==0) ? 'right' : 'left'; ?>-only<?php echo (get_sub_field('title_text')) ? ' section__text--grow-md-top' : ''; echo (get_sub_field('col_font')>0) ? ' section__text--alternate': ''; ?>">
 		<?php the_sub_field('text'); ?>
 	</div>
-	<?php } if(get_field('project_cat')) :
+	<?php } 
 		$cat = '';
 		if(get_post_type() == 'lampade') {
 			$term = wp_get_post_terms( $post->ID, 'collezioni' )[0];
@@ -27,7 +27,7 @@
 	<div class="section__link section__link--grow-md-top section__link--shrink-<?php echo ($col%2==0) ? 'right' : 'left'; ?>-only">
 		<a href="<?php the_sub_field('col_link'); ?>" ui-sref="app.page({slug : '<?php echo basename(get_sub_field('col_link')); ?>'<?php echo $cat; ?>})" class="section__send"><?php the_sub_field('link_text'); ?></a>
 	</div>
-	<?php endif; if(get_sub_field('col_sign')) : ?>
+	<?php if(get_sub_field('col_sign')) : ?>
 		<p class="section__sign section__sign--grow-md-top section__sign--lighter section__link--shrink-<?php echo ($col%2==0) ? 'right' : 'left'; ?>-only"><?php the_sub_field('col_sign'); ?></p>
 	<?php endif; endif; ?>
 <div class="section__mask"></div>
