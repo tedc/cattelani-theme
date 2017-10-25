a406ce1be3fbc62982c54e3a18d4f9a168272dde
tedc <egrandinetti@gmail.com>
2017-10-25 19:32:38 +0200

    prova

--- templates/entry-meta-projects.php
+++ templates/entry-meta-projects.php
@@ -1,6 +1,8 @@
 <?php 
-if($_GET['lang']): 
-echo $_GET['lang']; endif; ?>
+global $sitepress;
+$lang = $_GET['lang'] ? $_GET['lang'] : 'it';
+$sitepress->switch_lang($lang, true);
+?>
 <aside class="meta meta--grow meta--gray meta--grid meta--shrink-fw">
 	<time class="meta__cell meta__cell--grow meta__cell--shrink-right-only meta__cell--s3" pudate datetime="<?php the_time('Y:m:d'); ?>">
 		<?php echo ucfirst(get_the_time('F Y')); ?>
