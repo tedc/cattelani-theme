38957745e21048f290e2ba40a308f53427974bcb
tedc <egrandinetti@gmail.com>
2017-10-25 19:47:39 +0200

    diocane

--- templates/entry-meta-projects.php
+++ templates/entry-meta-projects.php
@@ -4,7 +4,7 @@ $lang = $_GET['lang'] ? $_GET['lang'] : 'it';
 ?>
 <aside class="meta meta--grow meta--gray meta--grid meta--shrink-fw">
 	<time class="meta__cell meta__cell--grow meta__cell--shrink-right-only meta__cell--s3" pudate datetime="<?php the_time('Y:m:d'); ?>">
-		<?php echo ucfirst(get_option( 'date_format' ),date_i18n( strtotime( '11/15-1976' ))); ?>
+		<?php echo ucfirst(date_i18n(get_option( 'date_format' ), strtotime( '11/15-1976' ))); ?>
 	</time>
 	<?php if(get_field('luogo') || get_field('citta')) : ?>
 	<div class="meta__cell meta__cell--grow meta__cell--shrink-right-only meta__cell--s3">
