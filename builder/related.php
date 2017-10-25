1fbb4b3e7e579255d25daf512da5cf5b6a5092e8
tedc <egrandinetti@gmail.com>
2017-10-25 19:57:55 +0200

    prova

--- builder/related.php
+++ builder/related.php
@@ -1,8 +1,14 @@
-<?php if(get_field('related')) : 
+<?php global $sitepress;
+if(get_field('related')) : 
 ?>
 <div class="related related--shrink-fw related--grow-md related--gray" id="related_<?php the_ID(); ?>" ng-swiper>
 	<header class="related__header related__header--grow-md">
-		<h4 class="related__title related__title--aligncenter-medium-lighter"><?php _e('Le lampade utilizzate', 'catellani'); ?></h4>
+		<h4 class="related__title related__title--aligncenter-medium-lighter"><?php _e('Le lampade utilizzate', 'catellani'); ?>
+			
+			<?php 
+			do_action( 'wpml_register_single_string', 'catellani', 'Related', 'Le lampade utilizzate' );
+			echo apply_filters( 'wpml_translate_single_string', 'Le lampade utilizzate', 'catellani', 'Related', $sitepress->default_language() ); ?>
+		</h4>
 	</header>
 	<div class="related__items related__items--grow-md-bottom related__items--grow-top">
 		<ks-swiper-container slides-per-view="'auto'" show-nav-buttons="true" swiper="main" override-parameters="{'nextButton' : '#related_<?php the_ID(); ?> .icon-arrow--next', 'prevButton' : '#related_<?php the_ID(); ?> .icon-arrow--prev'}">
--- templates/entry-meta-projects.php
+++ templates/entry-meta-projects.php
@@ -1,11 +1,11 @@
 <?php 
 global $sitepress;
-$lang = $_GET['lang'] ? $_GET['lang'] : 'it';
 ?>
 <aside class="meta meta--grow meta--gray meta--grid meta--shrink-fw">
 	<time class="meta__cell meta__cell--grow meta__cell--shrink-right-only meta__cell--s3" pudate datetime="<?php the_time('Y:m:d'); ?>">
 		<?php echo ucfirst(date_i18n(get_option( 'date_format' ), strtotime( '11/15-1976' ))); ?>
 	</time>
+
 	<?php if(get_field('luogo') || get_field('citta')) : ?>
 	<div class="meta__cell meta__cell--grow meta__cell--shrink-right-only meta__cell--s3">
 		<?php the_field('luogo'); ?>, <?php the_field('citta'); ?>
