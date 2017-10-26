0497f09f30880ade1b041f5f80f8415cdf08c502
tedc <egrandinetti@gmail.com>
2017-10-26 11:28:01 +0200

    related

--- builder/related.php
+++ builder/related.php
@@ -5,7 +5,7 @@ if(get_field('related')) :
 ?>
 <div class="related related--shrink-fw related--grow-md related--gray" id="related_<?php the_ID(); ?>" ng-swiper>
 	<header class="related__header related__header--grow-md">
-		<h4 class="related__title related__title--aligncenter-medium-lighter"><?php echo my_wpml_string('Le lampade utilizzate'); ?></h4>
+		<h4 class="related__title related__title--aligncenter-medium-lighter"><?php echo my_wpml_string('Le lampade utilizzate', 'catellani'); ?></h4>
 	</header>
 	<div class="related__items related__items--grow-md-bottom related__items--grow-top">
 		<ks-swiper-container slides-per-view="'auto'" show-nav-buttons="true" swiper="main" override-parameters="{'nextButton' : '#related_<?php the_ID(); ?> .icon-arrow--next', 'prevButton' : '#related_<?php the_ID(); ?> .icon-arrow--prev'}">
