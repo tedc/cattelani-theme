ddb5ba731a057f011cc0534b5f2d4fd1cccad6a7
tedc <egrandinetti@gmail.com>
2017-10-26 11:18:23 +0200

    search and contact

--- templates/modal-search.php
+++ templates/modal-search.php
@@ -2,8 +2,8 @@
 	<div class="search">
 		<div class="search__filters search__filters--grid search__filters--grow-top"> 
 			<div class="search__select" click-outside="isSelect['collezioni']=false" ng-class="{'search__select--filled' : search.collezioni}" ng-click="$event.stopPropagation();isSelect['collezioni']=!isSelect['collezioni']">
-				<!-- <span class="search__value" ng-bind-html="(search.collezioni ? select['collezioni'] : '<?php echo my_wpml_string('Collezione', 'catellani'); ?>')"></span> -->
-				<span class="search__value"><?php echo my_wpml_string('Collezione', 'catellani'); ?></span>
+				<!-- <span class="search__value" ng-bind-html="(search.collezioni ? select['collezioni'] : '<?php _e('Collezione', 'catellani'); ?>')"></span> -->
+				<span class="search__value"><?php _e('Collezione', 'catellani'); ?></span>
 				<span class="search__icons">
 					<i class="icon-select"></i>
 				</span>
@@ -17,8 +17,8 @@
 			</div>
 
 			<div class="search__select" click-outside="isSelect['posizioni']=false" ng-class="{'search__select--filled' : search.posizioni}" ng-click="isSelect['posizioni']=!isSelect['posizioni']">
-				<!-- <span class="search__value" ng-bind-html="(search.posizioni ? select['posizioni'] : '<?php echo my_wpml_string('Tipologia', 'catellani'); ?>')"></span> -->
-				<span class="search__value"><?php echo my_wpml_string('Tipologia', 'catellani'); ?></span>
+				<!-- <span class="search__value" ng-bind-html="(search.posizioni ? select['posizioni'] : '<?php _e('Tipologia', 'catellani'); ?>')"></span> -->
+				<span class="search__value"><?php _e('Tipologia', 'catellani'); ?></span>
 				<span class="search__icons">
 					<i class="icon-select"></i>
 				</span>
@@ -32,8 +32,8 @@
 			</div>
 
 			<div class="search__select" click-outside="isSelect['fonti']=false" ng-class="{'search__select--filled' : search.fonti}" ng-click="isSelect['fonti']=!isSelect['fonti']">
-				<!-- <span class="search__value" ng-bind-html="(search.fonti ? select['fonti'] : '<?php echo my_wpml_string('Fonte luminosa', 'catellani'); ?>')"></span> -->
-				<span class="search__value"><?php echo my_wpml_string('Fonte luminosa', 'catellani'); ?></span>
+				<!-- <span class="search__value" ng-bind-html="(search.fonti ? select['fonti'] : '<?php _e('Fonte luminosa', 'catellani'); ?>')"></span> -->
+				<span class="search__value"><?php _e('Fonte luminosa', 'catellani'); ?></span>
 				<span class="search__icons">
 					<i class="icon-select"></i>
 				</span>
@@ -46,7 +46,7 @@
 				</ul>
 			</div>
 			<div class="search__select" click-outside="isOrder=false" ng-click="isOrder=!isOrder">
-				<span class="search__value"><?php echo my_wpml_string('Ordina', 'catellani'); ?>: {{orderValue()}}</span>
+				<span class="search__value"><?php _e('Ordina', 'catellani'); ?>: {{orderValue()}}</span>
 				<span class="search__icons">
 					<i class="icon-select"></i>
 				</span>
@@ -57,14 +57,14 @@
 			</div>
 		</div>
 		<div class="search__results" ng-if="select.collezioni.length > 0 || select.posizioni.length > 0 || select.fonti.length > 0">
-			<div class="search__result" ng-if="select.collezioni.length > 0"><span><?php echo my_wpml_string('Collezioni'); ?>:</span> <strong ng-repeat="c in select.collezioni">{{($index > 0) ? ', ' : ' '}}{{c}} <i class="icon-close" ng-click="clear(c)"></i></strong></div>
-			<div class="search__result" ng-if="select.posizioni.length > 0"><span class="search__sep" ng-if="select.collezioni.length > 0">&mdash;</span><span><?php echo my_wpml_string('Tipologie', 'catellani'); ?>:</span> <strong ng-repeat="p in select.posizioni">{{($index > 0) ? ', ' : ' '}}{{p}} <i class="icon-close" ng-click="clear( p)"></i></strong></div>
-			<div class="search__result" ng-if="select.fonti.length > 0"><span class="search__sep" ng-if="select.collezioni.length > 0 || select.posizioni.length > 0">&mdash;</span><span><?php echo my_wpml_string('Fonti'); ?>:</span> <strong ng-repeat="f in select.fonti">{{($index > 0) ? ', ' : ' '}}{{f}} <i class="icon-close" ng-click="clear(f)"></i></strong></div>
+			<div class="search__result" ng-if="select.collezioni.length > 0"><span><?php _e('Collezioni'); ?>:</span> <strong ng-repeat="c in select.collezioni">{{($index > 0) ? ', ' : ' '}}{{c}} <i class="icon-close" ng-click="clear(c)"></i></strong></div>
+			<div class="search__result" ng-if="select.posizioni.length > 0"><span class="search__sep" ng-if="select.collezioni.length > 0">&mdash;</span><span><?php _e('Tipologie', 'catellani'); ?>:</span> <strong ng-repeat="p in select.posizioni">{{($index > 0) ? ', ' : ' '}}{{p}} <i class="icon-close" ng-click="clear( p)"></i></strong></div>
+			<div class="search__result" ng-if="select.fonti.length > 0"><span class="search__sep" ng-if="select.collezioni.length > 0 || select.posizioni.length > 0">&mdash;</span><span><?php _e('Fonti'); ?>:</span> <strong ng-repeat="f in select.fonti">{{($index > 0) ? ', ' : ' '}}{{f}} <i class="icon-close" ng-click="clear(f)"></i></strong></div>
 
 		</div>
 		<div class="search__items search__items--shrink-fw" scrollbar="search" speed="1.5">
 			<p class="search__cell search__cell--found search__cell--grow-md" ng-if="filtered() && isLoading">
-				<span><?php echo my_wpml_string('Nessuna lampada per la ricerca effettuata', 'catellani'); ?></span>
+				<span><?php _e('Nessuna lampada per la ricerca effettuata', 'catellani'); ?></span>
 			</p>
 			<a class="search__cell search__cell--grow-md" ng-repeat="item in items | taxSearch:search | orderBy:order" ui-sref="app.page({slug : item.slug, lang : '<?php echo ICL_LANGUAGE_CODE; ?>'})" on-finish-render="search_ended">
 				<span class="search__content">
