a2c968ff6d01df0b5a6f8c47a6db6c25d553c7b4
tedc <egrandinetti@gmail.com>
2017-10-30 19:25:47 +0100

    prova

--- extras/setup.php
+++ extras/setup.php
@@ -202,6 +202,21 @@
 
 	// }
 
+	function cleanup_default_rewrite_rules( $rules ) {
+	    foreach ( $rules as $regex => $query ) {
+	        if ( strpos( $regex, 'attachment' ) || strpos( $query, 'attachment' ) ) {
+	            unset( $rules[ $regex ] );
+	        }
+	    }
+
+	    return $rules;
+	}
+	add_filter( 'rewrite_rules_array', 'cleanup_default_rewrite_rules' );
+
+	function cleanup_attachment_link( $link ) {
+	    return;
+	}
+	add_filter( 'attachment_link', 'cleanup_attachment_link' );
 
 	add_action( 'init', function() {
 
