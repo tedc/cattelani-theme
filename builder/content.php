524ea3089a48b7b5f24d73c62da047436f93708b
tedc <egrandinetti@gmail.com>
2017-10-31 13:21:21 +0100

    container

--- builder/content.php
+++ builder/content.php
@@ -1,13 +1,13 @@
 <?php if(get_post_type() != 'post') : ?>
 <?php get_template_part( 'templates/entry-meta', 'projects' ); ?>
 <?php endif; ?>
-<article class="container container--<?php echo get_post_type(); ?> container--grow-lg container--grow-lg-shrink-fw" id="container_<?php the_ID(); ?>">	
+<article class="container container--<?php echo get_post_type(); ?> container--grow-md container--grow-lg-shrink-fw" id="container_<?php the_ID(); ?>">	
 	<?php if(get_post_type() == 'post') : ?>
 	<?php get_template_part( 'templates/entry', 'meta' ); ?>
 	<div class="container__middle">
 	<h1 class="container__title container__title--huge"><?php the_title(); ?></h1>
 	<?php endif; ?>
-	<div class="container__content container__content--grow-md-top container__content--mw">
+	<div class="container__content container__content--mw">
 		<?php the_field('content'); ?>
 	</div>
 	<?php if(get_post_type() == 'post') :  ?>
