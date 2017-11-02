ee804bcd9c284e4ccce6bb97ac8c8d8cb3385957
tedc <egrandinetti@gmail.com>
2017-11-02 16:41:13 +0100

    share

--- templates/entry-meta-social.php
+++ templates/entry-meta-social.php
@@ -1,6 +1,4 @@
 <nav class="container__share"<?php //scrollmagic('"pin":{"pushFollowers":false},"triggerElement":"#container_'.get_the_ID().'","triggerHook":"onLeave","duration":"100%","durationElement":"#container_'.get_the_ID().'"'); ?>>
-	<a href="" class="icon-facebook"></a>
-	<a href="" class="icon-googleplus"></a>
-	<a href="" class="icon-twitter"></a>
-	<a href="" class="icon-pinterest"></a>
+	<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" class="icon-facebook" target="_blank"></a>
+	<a href="http://twitter.com/share?url=<?php the_permalink(); ?>" class="icon-twitter" target="_blank"></a>
 </nav>
