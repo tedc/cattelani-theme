aedcc39b1a1889ac5c8c320e61efcbcae24cad9c
tedc <egrandinetti@gmail.com>
2017-11-02 17:21:00 +0100

    post_name

--- templates/next.php
+++ templates/next.php
@@ -12,7 +12,16 @@
 	<span class="next__container next__container--shrink-fw next__container--grow-md">
 	<span class="next__label next__label--grow-md-bottom"><?php 
 	$prossimo = ($post_type == 'post' || $post_type == 'progetti') ? __('Prossimo', 'catellani') : __('Prossima', 'catellani');
-	echo $prossimo . ' ' .strtolower($post_type_object->labels->singular_name); ?></span>
+	if($post_type == 'post') {
+		$post_name = __('articolo', 'catellani');
+	} elseif($post_type == 'lampade') {
+		$post_name = __('lampada', 'catellani');
+	} elseif($post_type == 'installazioni') {
+		$post_name = __('installazione', 'catellani');
+	} elseif($post_type == 'progetti') {
+		$post_name = __('progetto', 'catellani');
+	} 
+	echo $prossimo . ' ' .$post_name; ?></span>
 	<span class="next__title next__title--medium-alternate"><?php echo get_the_title($next->ID); ?>
 	<?php echo ($post_type == 'progetti' || $post_type == 'installazioni') ? '<br/><span class="next__city">'.get_field('city', $next_id).'</span>' : '' ; ?>
 	</span>
