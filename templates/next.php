8f680f6c6b419a615746e183e3d7f8b0203e9438
tedc <egrandinetti@gmail.com>
2017-11-03 14:51:08 +0100

    prova

--- templates/next.php
+++ templates/next.php
@@ -1,33 +1,33 @@
 <?php
-	global $APTO;
+	//global $APTO;
 	$term = wp_get_post_terms( $post->ID, 'collezioni' );
-	$args   =   array(
-                    '_adminsort' =>  array('yes')  
-                    );
+	// $args   =   array(
+ //                    '_adminsort' =>  array('yes')  
+ //                    );
 
-	//create the arguments of a query
-	$query  =   new stdClass();
-	//set the post type
-	$query->query_vars['post_type'] =   'lampade';
-	//set taxonomy
-	$query->tax_query->queries['relation']  =   'AND';
-	$query->tax_query->queries[]    =   array(
-	                                            'taxonomy'  =>   'collezioni',
-	                                            'field'     =>   'term_id',
-	                                            'terms'     =>   array( $term[0]->term_id ),
-	                                            'operator'  =>  'IN'
-	                                            );
+	// //create the arguments of a query
+	// $query  =   new stdClass();
+	// //set the post type
+	// $query->query_vars['post_type'] =   'lampade';
+	// //set taxonomy
+	// $query->tax_query->queries['relation']  =   'AND';
+	// $query->tax_query->queries[]    =   array(
+	//                                             'taxonomy'  =>   'collezioni',
+	//                                             'field'     =>   'term_id',
+	//                                             'terms'     =>   array( $term[0]->term_id ),
+	//                                             'operator'  =>  'IN'
+	//                                             );
 	                    
-	$sort_view_id   =   $APTO->functions->query_match_sort_id($query, $args);
+	// $sort_view_id   =   $APTO->functions->query_match_sort_id($query, $args);
 
-	$sort_view_post     =   get_post($sort_view_id);
-	$sortID             =   $sort_view_post->post_parent;
-	var_dump($sortID);
-	$post_type = get_post_type();
-	$post_type_object = get_post_type_object( $post_type );
+	// $sort_view_post     =   get_post($sort_view_id);
+	// $sortID             =   $sort_view_post->post_parent;
+	// var_dump($sortID);
+	// $post_type = get_post_type();
+	// $post_type_object = get_post_type_object( $post_type );
 	
-	//$next = ($post_type == 'post' || $post_type == 'progetti' || $post_type == 'installazioni' ) ? get_previous_post() : get_previous_post( true, null, 'collezioni');
-	$next = ($post_type == 'post' || $post_type == 'progetti' || $post_type == 'installazioni' ) ? get_previous_post() : apto_get_adjacent_post( array('sort_id' => $sortID, 'taxonomy' => 'collezioni', 'term_id' => $term[0]->term_id), true);
+	$next = ($post_type == 'post' || $post_type == 'progetti' || $post_type == 'installazioni' ) ? get_previous_post() : get_next_post( true, null, 'collezioni');
+	//$next = ($post_type == 'post' || $post_type == 'progetti' || $post_type == 'installazioni' ) ? get_previous_post() : apto_get_adjacent_post( array('sort_id' => $sortID, 'taxonomy' => 'collezioni', 'term_id' => $term[0]->term_id), true);
 	if($next) :
 	$next_id = id_by_lang($next->ID, get_post_type(), ICL_LANGUAGE_CODE);
 ?>
