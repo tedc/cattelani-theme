5662da5b7ac38c45b6723669ec2f0ad2f918aea3
tedc <egrandinetti@gmail.com>
2017-11-03 10:09:05 +0100

    get_previous_post

--- templates/next.php
+++ templates/next.php
@@ -1,7 +1,7 @@
 <?php
 	$post_type = get_post_type();
 	$post_type_object = get_post_type_object( $post_type );
-	$next = ($post_type == 'post' || $post_type == 'progetti' || $post_type == 'installazioni' ) ? get_previous_post() : get_next_post( true, null, 'collezioni');
+	$next = ($post_type == 'post' || $post_type == 'progetti' || $post_type == 'installazioni' ) ? get_previous_post() : get_previous_post( true, null, 'collezioni');
 	//$term = wp_get_post_terms( $post->ID, 'collezioni' );
 	//$next = ($post_type == 'post' || $post_type == 'progetti' || $post_type == 'installazioni' ) ? get_previous_post() : apto_get_adjacent_post( array('taxonomy' => 'collezioni', 'term_id' => $term[0]->term_id), true);
 	if($next) :
