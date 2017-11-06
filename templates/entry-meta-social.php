22f4c933c3c09410bd401e63f5a2997a6b6f67ee
tedc <egrandinetti@gmail.com>
2017-11-06 12:21:12 +0100

    log transition params

--- templates/entry-meta-social.php
+++ templates/entry-meta-social.php
@@ -1,4 +1,4 @@
 <nav class="container__share"<?php //scrollmagic('"pin":{"pushFollowers":false},"triggerElement":"#container_'.get_the_ID().'","triggerHook":"onLeave","duration":"100%","durationElement":"#container_'.get_the_ID().'"'); ?>>
 	<a href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>" class="icon-facebook" target="_blank"></a>
-	<a href="http://twitter.com/share?url=<?php the_permalink(); ?>" class="icon-twitter" target="_blank"></a>
+	<!-- <a href="http://twitter.com/share?url=<?php the_permalink(); ?>" class="icon-twitter" target="_blank"></a> -->
 </nav>
