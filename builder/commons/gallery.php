862fadff73940be967b11122e0e836b0864a6840
tedc <egrandinetti@gmail.com>
2017-11-08 17:10:42 +0100

    gallery varie

--- builder/commons/gallery.php
+++ builder/commons/gallery.php
@@ -8,8 +8,8 @@
 ?>
 <ng-swiper>
 <ks-swiper-container swiper="main" override-parameters="{'effect':'fade', 'fade' : {'crossFade' : true}}" show-nav-buttons="<?php echo count($images) > 1 ? 'true' : 'false'; ?>" ng-init="slider = [<?php echo $imgs; ?>]" loop="true" autoplay="5000">
-	<ks-swiper-slide class="swiper-slide" ng-repeat="img in slider" ng-style="{'background-image':'url({{img.url}})'}" ng-lazy-img="{{img.url}}">
-		<figure class="swiper-slider__figure"><img ng-src="{{img.url}}" ng-attr-alt="{{img.alt}}" /></figure>
+	<ks-swiper-slide class="swiper-slide" ng-repeat="img in slider" ng-lazy-img="{{img.url}}">
+		<figure class="swiper-slider__figure" ng-style="{'background-image':'url({{img.url}})'}"><img ng-src="{{img.url}}" ng-attr-alt="{{img.alt}}" /></figure>
 	</ks-swiper-slide>
 </ks-swiper-container>
 </ng-swiper>
