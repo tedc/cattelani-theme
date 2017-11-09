682824b9dd66b5599cb857c530cbb91734aed6ec
tedc <egrandinetti@gmail.com>
2017-11-09 12:02:49 +0100

    start_name

--- extras/base.php
+++ extras/base.php
@@ -10,6 +10,10 @@ use Roots\Sage\Wrapper;
   <?php get_template_part('templates/head'); 
   ?>
   <body ng-class="[body_class, isAnim]" <?php body_class(); ?>>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K7JJCPB"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <!--[if IE]>
       <div class="ie-alert ie-alert-warning">
         <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'catellani'); ?>
