d56cbc13747c2d584328360a4acc2dd57c1c576c
tedc <egrandinetti@gmail.com>
2017-11-10 19:15:24 +0100

    stream

--- extras/pdf.php
+++ extras/pdf.php
@@ -13,7 +13,7 @@
 		$dompdf->render();
 
 		// Output the generated PDF to Browser
-		$dompdf->output($title);
+		$dompdf->stream($title);
 	}
 	add_action( 'wp_ajax_catellanipdf', 'lamp_pdf', 10, 1 );
 
