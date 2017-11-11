cbaffeb6daa5e2c0f5c9480cd3bb043b56d77297
tedc <egrandinetti@gmail.com>
2017-11-11 02:24:22 +0100

    target blank remoe

--- templates/modal-languages.php
+++ templates/modal-languages.php
@@ -12,7 +12,7 @@
             $language_code = $l['language_code'];
                 
 		?>
-		<a href="<?php echo $language_link;  ?>" class="modal__button modal__button--lang modal__button--lang-upper<?php  echo ($language_code == ICL_LANGUAGE_CODE) ? ' modal__button--lang-active' : ''; ?>" target="_blank" ng-if="!lang_menu"><span class="lang"><?php echo $language_code; ?></span></a>
+		<a href="<?php echo $language_link;  ?>" class="modal__button modal__button--lang modal__button--lang-upper<?php  echo ($language_code == ICL_LANGUAGE_CODE) ? ' modal__button--lang-active' : ''; ?>" ng-if="!lang_menu"><span class="lang"><?php echo $language_code; ?></span></a>
 		<?php endforeach; ?>	
 		<a ng-href="{{l.href}}" class="modal__button modal__button--lang modal__button--lang-upper" ng-class="{'modal__button--lang-active':l.lang == '<?php echo ICL_LANGUAGE_CODE; ?>'}" ng-if="lang_menu" ng-repeat="l in lang_menu"><span class="lang" ng-bind-html="l.lang"></span></a>
 	</nav>
