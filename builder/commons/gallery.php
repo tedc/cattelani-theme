fa5b9c784f5870079c90bde01e1dd0e10e1047ea
tedc <egrandinetti@gmail.com>
2017-11-15 17:07:18 +0100

    gallery

--- builder/commons/gallery.php
+++ builder/commons/gallery.php
@@ -7,7 +7,7 @@
 	$count++; endforeach;
 ?>
 <ng-swiper>
-<ks-swiper-container swiper="main" show-nav-buttons="<?php echo count($images) > 1 ? 'true' : 'false'; ?>" ng-init="slider = [<?php echo $imgs; ?>]" loop="true"<?php if(count($images) > 1): ?> autoplay="5000"<?php endif; ?>>
+<ks-swiper-container swiper="main" override-parameters="{'effect':'fade', 'fade' : {'crossFade' : true}}" show-nav-buttons="<?php echo count($images) > 1 ? 'true' : 'false'; ?>" ng-init="slider = [<?php echo $imgs; ?>]" loop="true"<?php if(count($images) > 1): ?> autoplay="5000"<?php endif; ?>>
 	<ks-swiper-slide class="swiper-slide" ng-repeat="img in slider">
 		<figure class="swiper-slide__figure" ng-style="{'background-image':'url({{img.url}})'}"><img ng-src="{{img.url}}" ng-attr-alt="{{img.alt}}" /></figure>
 	</ks-swiper-slide>
