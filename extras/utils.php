9bfd5dbd76903bf4f1ee8a64af8b17a163fb6227
tedc <egrandinetti@gmail.com>
2017-11-30 18:32:05 +0100

    random nonce

--- extras/utils.php
+++ extras/utils.php
@@ -127,7 +127,15 @@
 		return $data;
 	}
 
-
+	function generateRandomString($length = 10) {
+	    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	    $charactersLength = strlen($characters);
+	    $randomString = '';
+	    for ($i = 0; $i < $length; $i++) {
+	        $randomString .= $characters[rand(0, $charactersLength - 1)];
+	    }
+	    return $randomString;
+	}
 	// function my_next_post_where() {
 
 	// 	global $post, $wpdb;
