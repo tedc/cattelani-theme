bb53a878e79ebee0d17d0c1b39a13da2cdd33282
tedc <egrandinetti@gmail.com>
2017-12-01 12:50:10 +0100

    next

--- templates/next.php
+++ templates/next.php
@@ -27,12 +27,12 @@
 	// $post_type_object = get_post_type_object( $post_type );
 	
 	//$next = ($post_type == 'post' || $post_type == 'progetti' || $post_type == 'installazioni' ) ? get_previous_post() : get_previous_post( true, null, 'collezioni');
-	$next = ($post_type == 'post' || $post_type == 'progetti' || $post_type == 'installazioni' ) ? get_previous_post() : apto_get_adjacent_post( array('taxonomy' => 'collezioni', 'term_id' => $term[0]->term_id), true);
+	$next = (get_post_type() == 'post' || get_post_type() == 'progetti' || get_post_type() == 'installazioni' ) ? get_previous_post() : apto_get_adjacent_post( array('taxonomy' => 'collezioni', 'term_id' => $term[0]->term_id), true);
 	if($next) :
 	$next_id = id_by_lang($next->ID, get_post_type(), ICL_LANGUAGE_CODE);
 ?>
-<a class="next next--grow-lg next--<?php echo $post_type; ?>" next-element href="<?php echo get_permalink($next->ID); ?>" ui-sref="app.page({slug : '<?php echo basename(get_permalink($next->ID)); ?>', lang : '<?php echo ICL_LANGUAGE_CODE; ?>'})">
-	<span class="next__cover" ng-style="{'background-image':'url(<?php echo get_the_post_thumbnail_url($next->ID, 'full'); ?>)'}"></span>
+<a class="next next--grow-lg next--<?php echo $post_type; ?>" next-element href="<?php echo get_permalink($next_id); ?>" ui-sref="app.page({slug : '<?php echo basename(get_permalink($next_id)); ?>', lang : '<?php echo ICL_LANGUAGE_CODE; ?>'})">
+	<span class="next__cover" ng-style="{'background-image':'url(<?php echo get_the_post_thumbnail_url($next_id, 'full'); ?>)'}"></span>
 	<span class="next__container next__container--shrink-fw next__container--grow-md">
 	<span class="next__label next__label--grow-md-bottom"><?php 
 	$prossimo = ($post_type == 'post' || $post_type == 'progetti') ? __('Prossimo', 'catellani') : __('Prossima', 'catellani');
@@ -46,7 +46,7 @@
 		$post_name = __('progetto', 'catellani');
 	} 
 	echo $prossimo . ' ' .$post_name; ?></span>
-	<span class="next__title next__title--medium-alternate"><?php echo get_the_title($next->ID); ?>
+	<span class="next__title next__title--medium-alternate"><?php echo get_the_title($next_id); ?>
 	<?php echo ($post_type == 'progetti' || $post_type == 'installazioni') ? '<br/><span class="next__city">'.get_field('city', $next_id).'</span>' : '' ; ?>
 	</span>
 	</span>
