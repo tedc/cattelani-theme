1d27d99fb759a63e42c31e10be8fd6497ac49ebe
tedc <egrandinetti@gmail.com>
2017-12-05 16:19:35 +0100

    next id

---
+++ builder/commons/gallery.php
@@ -0,0 +1,17 @@
+<?php
+	if($images):
+		var_dump($images);
+	$imgs = '';
+	$count=0; foreach($images as $img) :
+	$comma = ($count>0) ? ',' : '';
+	$imgs .= ($full) ? $comma. '{url : \''.$img['url'].'\',alt : \''.$img['alt'].'\'}' : $comma. '{url : \''.$img['url'].'\'}';
+	$count++; endforeach;
+?>
+<ng-swiper>
+<ks-swiper-container swiper="main" override-parameters="{'effect':'fade', 'fade' : {'crossFade' : true}}" show-nav-buttons="<?php echo count($images) > 1 ? 'true' : 'false'; ?>" ng-init="slider = [<?php echo $imgs; ?>]" loop="true"<?php if(count($images) > 1): ?> autoplay="5000"<?php endif; ?>>
+	<ks-swiper-slide class="swiper-slide" ng-repeat="img in slider">
+		<figure class="swiper-slide__figure" ng-style="{'background-image':'url({{img.url}})'}"><img ng-src="{{img.url}}" ng-attr-alt="{{img.alt}}" /></figure>
+	</ks-swiper-slide>
+</ks-swiper-container>
+</ng-swiper>
+<?php endif; ?>
