53f2981c52ebfbf49c79d605fc05d9b5934033d6
tedc <egrandinetti@gmail.com>
2017-12-05 19:09:35 +0100

    var_dump(get_previous_post());

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,2 +1,2 @@
-<?php while (have_posts()) : the_post(); ?><?php the_content(); ?><?php endwhile; var_dump(get_previous_post());
+<?php while (have_posts()) : the_post(); ?><?php the_content(); var_dump(get_previous_post()); ?><?php endwhile; 
 	?>
