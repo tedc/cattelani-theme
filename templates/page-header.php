8806b0d613ed971439be2c87efc8d52aa808f1c5
tedc <egrandinetti@gmail.com>
2017-12-06 15:36:04 +0100

    image

--- templates/page-header.php
+++ templates/page-header.php
@@ -8,6 +8,7 @@
 	$the_id = ($obj) ? $obj->taxonomy.'_'.$the_id : $the_id;
 	$kind = get_field('header_kind');
 	$cover = get_field('featured_image', $the_id) ? get_the_post_thumbnail_url($the_id, 'full') : get_field('cover_image', $the_id)['url'];
+	$alt = get_field('featured_image', $the_id) ? get_post_meta(get_post_thumbnail_id($the_id), '_wp_attachment_image_alt', true) : Titles\title();
 	$className  = $kind == 0 ? 'header--bg header--bg-'. get_post_type() : 'header--white';
 	$data = '';
 	if(get_post_type() == 'lampade')  :
@@ -17,7 +18,9 @@
 
 <header class="header header--shrink header--<?php echo get_post_type(); ?> <?php echo $className; ?> <?php echo ($kind == 0) ? 'header--grow-md-bottom' : 'header--grow-lg-bottom'; ?>"<?php echo $data; ?>>
 	<?php if( $kind == 0 ): ?>
-	<div class="header__cover" ng-style="{'background-image' : 'url(<?php echo $cover; ?>)'}"></div>
+	<div class="header__cover" ng-style="{'background-image' : 'url(<?php echo $cover; ?>)'}">
+		<img src="<?php echo $cover; ?>" alt="<?php echo $alt; ?>" class="seo-image" />
+	</div>
 	<?php endif; ?>
 	<?php //if(get_post_type() != 'post') : ?>
 	<h1 class="header__title<?php echo (get_field('is_scroller_hidden', $the_id) || (get_post_type() == 'post' || get_post_type() == 'progetti'  || get_post_type() == 'installazioni' )) ? ' header__title--noscroller' : ''; ?> <?php echo ($kind == 0) ? 'header__title--medium' : 'header__title--large-lighter'; ?>"><?php
