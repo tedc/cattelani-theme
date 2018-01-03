1a490c0b96f8c08e97e972cf494f16aa2abd6b4b
tedc <egrandinetti@gmail.com>
2018-01-03 19:15:44 +0100

    prova

--- templates/next.php
+++ templates/next.php
@@ -30,9 +30,8 @@
 	
 	//$next = ($post_type == 'post' || $post_type == 'progetti' || $post_type == 'installazioni' ) ? get_previous_post() : get_previous_post( true, null, 'collezioni');
 
-	$next = (get_post_type() == 'lampade' ) ? apto_get_adjacent_post( array('taxonomy' => 'collezioni', 'term_id' => $term[0]->term_id), true) : get_previous_post();
-	
-	var_dump(apto_get_adjacent_post( FALSE, FALSE, FALSE, TRUE)->ID);
+	$next = (get_post_type() == 'lampade' ) ? apto_get_adjacent_post( array('taxonomy' => 'collezioni', 'term_id' => $term[0]->term_id), true) : apto_get_adjacent_post( FALSE, FALSE, FALSE, TRUE);
+
 	if($next) :
 	$next_id = (ICL_LANGUAGE_CODE != $sitepress->get_default_language()) ? apply_filters('wpml_object_id', $next->ID, get_post_type(), false, ICL_LANGUAGE_CODE) : $next->ID;
 	if($next_id) :
