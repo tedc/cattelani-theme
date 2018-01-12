93e55c18f533325cfb3fca14a28bd4641c2b36da
tedc <egrandinetti@gmail.com>
2018-01-12 12:25:56 +0100

    header

--- extras/scripts.php
+++ extras/scripts.php
@@ -123,6 +123,15 @@
 	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 	})(window,document,'script','dataLayer','GTM-K7JJCPB');</script>
 	<!-- End Google Tag Manager -->
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-29668127-1"></script>
+<script>
+ window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);}
+ gtag('js', new Date());
+
+ gtag('config', 'UA-29668127-1');
+</script>
 	<!-- Facebook Pixel Code -->
 <script>
  !function(f,b,e,v,n,t,s)
