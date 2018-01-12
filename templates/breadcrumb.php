8ed6b2a59f412937dc363007b2b0c0ce8a65fdd2
tedc <egrandinetti@gmail.com>
2018-01-12 10:30:30 +0100

    breadcrumb schema

--- templates/breadcrumb.php
+++ templates/breadcrumb.php
@@ -18,24 +18,25 @@
 		if($post_type != 'page') :
 			if($post_type != 'lampade') :
 				$the_index_id = id_by_lang($index[$post_type], 'page', $lang);
-				$ancestor = ($the_index_id) ? '<a ui-sref="app.page({slug : \''.basename(get_permalink($the_index_id)).'\', lang : \''.$lang.'\'})">'.get_the_title($the_index_id).'</a>'.$sep : '';
+				$ancestor = ($the_index_id) ? '<span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="'.get_permalink($the_index_id).'" ui-sref="app.page({slug : \''.basename(get_permalink($the_index_id)).'\', lang : \''.$lang.'\'})"><span itemprop="name">'.get_the_title($the_index_id).'</span></a><meta itemprop="position" content="2" /></span>'.$sep : '';
 			else :
 				$term = wp_get_post_terms( $post_obj->ID, 'collezioni' )[0];
 				$term = get_term(id_by_lang($term->term_id, 'collezioni', $lang));
-				$ancestor = ($term) ? '<a href="" ui-sref="app.collection({name : \''.basename(get_term_link($term->term_id)).'\', lang : \''.$lang.'\'})">'.$term->name.'</a>'.$sep : '';
+				$ancestor = ($term) ? '<span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="'.get_permalink($the_index_id).'" ui-sref="app.collection({name : \''.basename(get_term_link($term->term_id)).'\', lang : \''.$lang.'\'})"><span itemprop="name">'.$term->name.'</span></a><meta itemprop="position" content="2" /></span>'.$sep : '';
 			endif;
 		endif;
 		$current = ($post_id) ? '<span>'.get_the_title($post_id).'</span>' : '';
 	elseif($term_obj) :
-		$ancestor = ($term_obj->taxonomy == get_option('glossary-settings')['slug-cat']) ? '<a ui-sref="app.page({slug : \''.basename(get_permalink($index['glossary'])).'\', lang : \''.$lang.'\'})">'.get_the_title(id_by_lang($index['glossary'], 'page', $lang)).'</a>'.$sep : '';
+		$ancestor = ($term_obj->taxonomy == get_option('glossary-settings')['slug-cat']) ? '<span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="'.get_permalink($index['glossary']).'" ui-sref="app.page({slug : \''.basename(get_permalink($index['glossary'])).'\', lang : \''.$lang.'\'})"><span itemprop="name">'.get_the_title(id_by_lang($index['glossary'], 'page', $lang)).'</span></a><meta itemprop="position" content="2" /></span>'.$sep : '';
 		$term = get_term(id_by_lang($term_obj->term_id, $term_obj->taxonomy, $lang));
-		$current = ($term) ? $ancestor.'<span>'.$term->name.'</span>' : '';
+		$current = ($term) ? $ancestor.'<span itemprop="name">'.$term->name.'</span>' : '';
 	endif;
 	if($current != '') :
-		if($post_obj && !$term_obj) :
+		$current = ($ancestor != '') ? '<span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><span itemprop="item">'.$current.'<meta itemprop="position" content="3" /></span></span>' : '<span itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><span itemprop="item">'.$current.'<meta itemprop="position" content="2" /></span></span>';
+  		if($post_obj && !$term_obj) :
 			$post_id = id_by_lang($post_obj->ID, $post_type, $lang);
 			if($home != $post_id) :
-				echo '<a href="'.get_permalink($home).'" ui-sref="app.root({lang : \''.$lang.'\'})">Home</a>'.$sep.$ancestor.$current;
+				echo '<span  itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><a itemprop="item" href="'.get_permalink($home).'" ui-sref="app.root({lang : \''.$lang.'\'})"><span itemprop="name">Home</span></a><meta itemprop="position" content="1" /></span>'.$sep.$ancestor.$current;
 			endif;
 		else :
 			echo '<a href="'.get_permalink($home).'" ui-sref="app.root({lang : \''.$lang.'\'})">Home</a>'.$sep.$current;
