07cecb83f988f65376daffc105cd1fde913bdec8
tedc <egrandinetti@gmail.com>
2018-02-15 18:27:31 +0100

    remove autoresponse

--- extras/form.php
+++ extras/form.php
@@ -74,7 +74,7 @@
 	            //$transport = Swift_MailTransport::newInstance();
 	            $transport = Swift_SmtpTransport::newInstance('asms3.assolo.net', 465, 'ssl')->setUsername('contact@catellanismith.com')->setPassword('gyg725bdl');
 	            $mMailer = Swift_Mailer::newInstance($transport);
-	            $rEmail = Swift_Message::newInstance();
+	            //$rEmail = Swift_Message::newInstance();
 	            $mEmail = Swift_Message::newInstance();
 	            $mEmail->setSubject($pSubject);
 	            $mEmail->setTo($pTo);
@@ -82,10 +82,10 @@
 	            $mEmail->setFrom(array($email => $name));
 	            $mEmail->setReplyTo(array($email));
 	            $mEmail->setBody(template($body), 'text/html');
-	            $rEmail->setSubject(str_replace('&amp;', '&', $rSubject));
-	            $rEmail->setFrom(array($pTo => str_replace('&amp;', '&', get_bloginfo('name'))));
-	            $rEmail->setTo(array($email));
-	            $rEmail->setBody(template($resp), 'text/html');
+	            // $rEmail->setSubject(str_replace('&amp;', '&', $rSubject));
+	            // $rEmail->setFrom(array($pTo => str_replace('&amp;', '&', get_bloginfo('name'))));
+	            // $rEmail->setTo(array($email));
+	            // $rEmail->setBody(template($resp), 'text/html');
 	            if($mMailer->send($mEmail)){
 	            	$data = array('formMsg' => "<h3 class='form__title'>".$sent."</h3><p>".$tnx."</p><a ui-sref='app.root({lang : \"".ICL_LANGUAGE_CODE."\"})' class='form__send' href='".get_home_url()."'>".__('Torna alla home', 'catellani')."</a>");
 	                $data = array('formMsg' => "<h3 class='form__subtitle form__subtitle'>".__('Grazie per averci contattato', 'iro')."</h3><p>".__('Ti risponderemo nel più breve tempo possibile', 'iro')."</p><a ui-sref='app.root({lang : \"".ICL_LANGUAGE_CODE."\"})' class='form__button' href='".get_home_url()."'>".__('Torna allo shop', 'iro')."</a>", 'mc' => $MailChimp->get("lists/$list_id/members/$subscriber_hash"));
