1249d6df5900c8110814b7eac185bceadc6c4f32
tedc <egrandinetti@gmail.com>
2018-04-19 16:49:57 +0200

    prova

--- extras/setup.php
+++ extras/setup.php
@@ -786,3 +786,26 @@ function collezioni_og_type($type) {
 	return $type;
 }
 add_filter('wpseo_opengraph_type', 'collezioni_og_type');
+
+
+add_filter('mod_rewrite_rules', 'fix_rewritebase');
+function fix_rewritebase($rules){
+    $home_root = parse_url(home_url());
+    if ( isset( $home_root['path'] ) ) {
+        $home_root = trailingslashit($home_root['path']);
+    } else {
+        $home_root = '/';
+    }
+ 
+    $wpml_root = parse_url(get_option('home'));
+    if ( isset( $wpml_root['path'] ) ) {
+        $wpml_root = trailingslashit($wpml_root['path']);
+    } else {
+        $wpml_root = '/';
+    }
+ 
+    $rules = str_replace("RewriteBase $home_root", "RewriteBase $wpml_root", $rules);
+    $rules = str_replace("RewriteRule . $home_root", "RewriteRule . $wpml_root", $rules);
+ 
+    return $rules;
+}
